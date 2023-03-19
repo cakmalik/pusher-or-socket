bb45f5fac0d6057198f3a6823e9bda79a269ba8b
cakmalik <hasinilmalik@gmail.com>
2023-03-18 18:50:41 -0700

    first

---
+++ resources/js/my-pusher.js
@@ -0,0 +1,24 @@
+import Echo from "laravel-echo";
+
+import Pusher from "pusher-js";
+window.Pusher = Pusher;
+
+window.Echo = new Echo({
+    broadcaster: "pusher",
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? "mt1",
+    wsHost: import.meta.env.VITE_PUSHER_HOST
+        ? import.meta.env.VITE_PUSHER_HOST
+        : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
+    wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
+    wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
+    forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? "https") === "https",
+    enabledTransports: ["ws", "wss"],
+});
+
+var channel = window.Echo.channel("nama-channel");
+
+channel.listen("BuatPesan", (e) => {
+    console.log("Berhasil");
+    console.log(e);
+});
